753fee7ecf5ed3142bdd21300bc7377bd6f57c8a
Trong Dai <caotrongdai2004cd@gmail.com>
2026-02-26 21:53:00 +0700

    tính năng tạo mới sản phẩm

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,9 +2,9 @@ const Product = require("../../models/product.model")
 const filterStatusHelpers = require("../../helpers/filterStatus");
 const searchHelpers = require("../../helpers/search");
 const paginationHelpers = require("../../helpers/pagination");
+const systemConfiig = require("../../config/system")
 
-
-// [GET] /adfmin/products
+// [GET] /admin/products
 
 module.exports.index = async (req, res) => {
         const filterStatus = filterStatusHelpers(req.query);
@@ -140,4 +140,35 @@ module.exports.deleteItem = async (req, res) => {
 
          req.flash('success', `đã xóa thành công sản phẩm !`);    
         res.redirect(req.get("Referer"));
+}
+
+
+
+// [GET] /admin/products/create
+
+module.exports.create = async (req, res) => {
+      
+        res.render("admin/pages/products/create", {
+                pageTitle: "Thêm mới sản phẩm",
+             
+        });
+}
+
+// [POST] /admin/products/create
+
+module.exports.createPost = async (req, res) => {
+        req.body.price = parseInt(req.body.price);
+        req.body.discountPercentage = parseInt(req.body.discountPercentage);
+        req.body.stock = parseInt(req.body.stock);
+        if(req.body.position == ""){
+                const countProducts = await Product.countDocuments();
+                req.body.position = countProducts + 1
+                
+        }else{
+                req.body.position = parseInt(req.body.position);
+        }
+
+        const product = new Product(req.body);
+        await product.save();
+        res.redirect(`${systemConfiig.prefixAdmin}/products`)
 }
--- routes/admin/product.route.js
+++ routes/admin/product.route.js
@@ -7,7 +7,9 @@ router.get('/change-status/:status/:id', controller.changeStatus)
 
 router.patch('/change-multi', controller.changeMulti)
 router.delete('/delete/:id', controller.deleteItem);
+router.get('/create', controller.create )
 
+router.post('/create', controller.createPost )
 
 
 module.exports = router;
