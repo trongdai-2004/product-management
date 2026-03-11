3412e8c7c00d39549abcdd79232a450a55162f10
Trong Dai <caotrongdai2004cd@gmail.com>
2026-03-11 16:14:04 +0700

    hiển thị khi upload ảnh

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -172,3 +172,25 @@ if (showAlert){
 
 
 // end show-alert
+
+
+// upload img
+
+const uploadImage = document.querySelector("[upload-image]");
+if (uploadImage){
+    const uploadImageInput = document.querySelector("[upload-image-input]")
+    const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+
+    uploadImageInput.addEventListener("change", (e) =>{
+        console.log(e);
+
+        const file = e.target.files[0];
+        if(file){
+            uploadImagePreview.src = URL.createObjectURL(file);
+        }
+    });
+}
+
+
+// end upload img
